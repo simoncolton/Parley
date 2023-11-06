1c15751f4092c2c6be280d9462d0b92963b3c0e1
simoncolton <s.colton@qmul.ac.uk>
2023-11-06 16:27:54 +0100

    Added EditingUtils

---
+++ ParleyV2/Utils/EditingUtils.py
@@ -0,0 +1,16 @@
+from ParleyV2.Utils.ExtractionUtils import *
+
+class EditingUtils:
+
+    def change_note_pitch(composition, note, new_pitch):
+        note.pitch = new_pitch
+        track_num = composition.note_sequences_hash[note.note_sequence_num]
+        if note.tie_type == "start":
+            track_notes = ExtractionUtils.get_notes_for_track_num(composition, track_num)
+            tie_type = "x"
+            ind = track_notes.index(note) + 1
+            while tie_type != "start" and tie_type is not None and ind < len(track_notes):
+                track_notes[ind].pitch = note.pitch
+                track_notes[ind].score_colour = note.score_colour
+                tie_type = track_notes[ind].tie_type
+                ind += 1
